be0ccf43b30a70c0f2e775254b213f2015084b88
tuwnguyen <tuwnguyen@gmail.com>
2024-02-17 18:50:55 +0700

    using route params

--- src/components/PageViewer.vue
+++ src/components/PageViewer.vue
@@ -1,22 +1,14 @@
 <template>
   <div id="content" class="container">
-    <h1 class="title-color">{{page.pageTitle}}</h1>
-    <p>{{page.content}}</p>
+    <h1 class="title-color">Page title</h1>
+    <p>{{$route.params.index}}</p>
   </div>
 </template>
 <script>
 export default {
-  props: {
-    page: {
-      type: Object,
-      default(rawProps) {
-        return {
-          pageTitle: '',
-          content: '',
-        }
-      }
-    }
-  },
+  created() {
+    
+  }
 }
 </script>
 <style scoped>
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ import CreatePage from './components/CreatePage.vue';
 const router = createRouter({
   history: createWebHashHistory(),
   routes: [
-    { path: '/', component: PageViewer },
+    { path: '/:index?', component: PageViewer },
     { path: '/create', component: CreatePage },
   ],
 });
