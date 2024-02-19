d7712f8a748ac4757c63469ec3f5f1349615e6a5
tuwnguyen <tuwnguyen@gmail.com>
2024-02-20 00:12:35 +0700

    Composition API: setup, ref, reactive

--- src/views/PagesList.vue
+++ src/views/PagesList.vue
@@ -1,3 +1,16 @@
 <template>
-  <p>Pages list</p>
+  <p>{{ data.counter }}</p>
+  <button @click.prevent="click">Click</button>
 </template>
+
+<script setup>
+import {ref, reactive} from "vue"
+
+// const counter = ref(0) //{value: 0}
+const data = reactive({counter: 0})
+const click = () => {
+  // counter.value++
+  data.counter++
+}
+
+</script>
